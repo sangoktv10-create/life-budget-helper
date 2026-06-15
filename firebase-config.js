49276b79315017c57c9bd841459ced47bf302498
sangoktv10-create <sangoktv10@gmail.com>
2026-06-15 20:21:38 +0900

    Add Firebase configuration file

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyDJQ_-hra_RHZjFdwsy_t-AxV9-vGMQM9M",
+  authDomain: "life-budget-helper.firebaseapp.com",
+  projectId: "life-budget-helper",
+  storageBucket: "life-budget-helper.firebasestorage.app",
+  messagingSenderId: "428003708378",
+  appId: "1:428003708378:web:bd05745cd28e483802c4e3",
+  measurementId: "G-40VJ4XTBMK"
+};
